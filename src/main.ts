455d9c07a5b483505ba7c218f84a0f731172df51
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-07-28 23:57:55 +0100

    nestjs_ticketing_typeOrm_eith_docker

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    // origin: 'http://localhost:4200',
     origin: true,
     credentials: true,
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'], // Include 'PATCH' in the allowed methods
