9cd556a6219cd31339f1f47db3870404ed71abff
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-07-29 01:24:12 +0100

    adding response

--- src/department/department.controller.ts
+++ src/department/department.controller.ts
@@ -1,15 +1,20 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe} from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete,Res,HttpStatus, ParseUUIDPipe} from '@nestjs/common';
 import { DepartmentService } from './department.service';
 import { CreateDepartmentDto } from './dto/create-department.dto';
 import { UpdateDepartmentDto } from './dto/update-department.dto';
+import type { Response } from 'express';
 
 @Controller('department')
 export class DepartmentController {
   constructor(private readonly departmentService: DepartmentService) {}
 
   @Post('create')
-  create(@Body() createDepartmentDto: CreateDepartmentDto) {
-    return this.departmentService.create(createDepartmentDto);
+  create(@Body() createDepartmentDto: CreateDepartmentDto, @Res({ passthrough: true }) response: Response) {
+    let result= this.departmentService.create(createDepartmentDto);
+    return response.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      message: 'Dept successfully created',
+      result: result    });
   }
 
   @Get('all')
